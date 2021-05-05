a5fef1c2fe3037fb94e66f5f35cee8e47e3cced6
D-2658 <wato0326@gmail.com>
2021-05-05 15:34:26 +0900

    todoリストの追加用のフォーム

---
+++ SideBar_React/src/Component/TodoForm.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+function TodoForm({addTodo}){
+    const {value, setValue} = useState('');
+
+    const handleSubmit = event=>{
+        event.preventDefault();
+        addTodo(value).then(()=>{
+            setValue('');
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+          <input
+            // value が入力欄に描画される
+            value={value}
+            // 入力欄の値を変更したら value を更新する
+            onChange={event => setValue(event.target.value)}
+          />
+          <button type="submit">追加</button>
+        </form>
+      );
+}
+
+export default TodoForm;

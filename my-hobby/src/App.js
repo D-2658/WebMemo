37de563124ebb8b05919d9a22337d7f28240faec
D-2658 <wato0326@gmail.com>
2021-06-28 00:27:10 +0900

    my-hobby-backとaxiosでやり取り

--- my-hobby/src/App.js
+++ my-hobby/src/App.js
@@ -1,29 +1,19 @@
-import React, { Component} from "react";
-/* import axios from 'axios';*/
+import React, {useMemo, useState} from 'react';
+import axios from 'axios';
 
-function TweetSearch(){
-  const [tepm, settemp] = settemp("Hello ,World");
+function App(){
+  const [tepm, settemp] = useState("Hello ,World");
 
-/*   axios.get("http://localhost:5001/get")
+   axios.get("http://localhost:5001/get")
        .then(res =>{
-        this.settemp(res);
+        settemp(res);
        })
- */  
+ 
   return(
     <div>
-      aaaaaaaa
+      {tepm}
     </div>
   );
-}
-
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello,World </h1>
-      </div>
-    );
-  }
 }
 
 export default App;
